f0fcb0d492d6ab3e03e04846f0857d9a544971f4
Martin Perez <martinperezsh@gmail.com>
2017-07-20 16:17:24 -0600

    got everything but the radio buttons working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,30 @@
-var timer = 30;
-var rightAnswers;
-var wrongAnswers;
+// Starting at 31 because 30 wont appear on screen if its 30
+var timer = 31;
+var rightAnswers = 0;
+var wrongAnswers = 0;
 var intervalId;
-
-
+// getting value of radio buttons
+var valq1 = $('input:radio[name=q1]:checked').val();
+var valq2 = $('input:radio[name=q2]:checked').val();
+var valq3 = $('input:radio[name=q3]:checked').val();
+var valq4 = $('input:radio[name=q4]:checked').val();
+var valq5 = $('input:radio[name=q5]:checked').val();
 
 
 //------------------------------------------- Start game -------------------------------------
 $( document ).ready(function() {
-	intervalId = setInterval(decrement, 1000);
+// Makes it only show the start screen
+  $(".container").hide();
+  $("#scoreScreen").hide();
+  
+// Starts the trivia
+  $(".btn").on("click", function(){
+    $("#startScreen").hide();
+    $(".container").show();
+    intervalId = setInterval(decrement, 1000);
+  });
+  
+	
 
 // Countdonw Timer
 	function decrement() {
@@ -16,8 +32,48 @@ $( document ).ready(function() {
 
 		$("#gameTimer").html(timer)
 
+// Ends game and shows score
 		if (timer === 0) {
-			// Take to the score page
+			$(".container").hide();
+      $("#scoreScreen").show();
 		}
-	}
+	
+  }
+  
+//   Adds up the score
+  function score(){
+    if (valq1 === 1) {
+      rightAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+    if (valq2 === 1) {
+      rightAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+    if (valq3 === 1) {
+      rightAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+    if (valq4 === 1) {
+      rightAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+    if (valq5 === 1) {
+      rightAnswers++;
+    } else {
+      wrongAnswers++;
+    }
+    
+   console.log(rightAnswers + " correct");
+   console.log(wrongAnswers + " wrong");
+  }
+
+// Display score after game
+    $("#rightAnswers").html(rightAnswers);
+    $("#wrongAnswers").html(wrongAnswers);
+  
 });
